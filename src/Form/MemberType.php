30b743baa87180e507fc394e05b56f547bdfc304
mchr70 <mchr70@gmail.com>
2019-02-26 15:25:30 +0100

    Améliorations du front pour l'utilisateur connecté

--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
@@ -67,7 +68,7 @@ class MemberType extends AbstractType
                     'query_builder'=>function(GenreRepository $gr){
                         return $gr ->createQueryBuilder('g')->orderBy('g.name','ASC');}
                 ) 
-            )
+            )          
             ->add('submit', SubmitType::class, ['label' => 'Sauvegarder les modifications'])
         ;
     }
